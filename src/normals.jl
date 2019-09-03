4a4e29af976de52e08380d0dda6cebb01de3d7f0
Chad Scherrer <chad.scherrer@gmail.com>
2019-09-03 14:36:59 -0700

    add normals

--- src/normals.jl
+++ src/normals.jl
@@ -1,6 +1,3 @@
-
-
-
 # https://www.sciencedirect.com/science/article/pii/S0895717710005935
 
 function boxMuller(u,v)
@@ -9,10 +6,16 @@ function boxMuller(u,v)
     (r * cos(θ), r * sin(θ))
 end
 
+normals() = ()
 normals(x::Vector) = normals(x...)
 normals(u,v,args...) = (boxMuller(u,v)..., normals(args...)...)
 normals(u) = norminvcdf(u)
 
 export StdNormals
+export Normals
 
 StdNormals(N) = Quasi(N,normals)
+
+Normals(N,μ,σ) = map(x -> σ*x + μ, StdNormals(N))
+
+# MvNormals(X)
